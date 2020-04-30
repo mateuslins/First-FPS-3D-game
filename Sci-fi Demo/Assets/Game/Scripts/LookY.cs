0775b5e831dd751b32ee0d17a8dda0b5584c0420
mateuslins <41246021+mateuslins@users.noreply.github.com>
2020-04-30 16:03:26 -0300

    Player movement implemented

---
+++ Sci-fi Demo/Assets/Game/Scripts/LookY.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookY : MonoBehaviour
+{
+    [SerializeField]
+    private float _sensitivity = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _mouseY = Input.GetAxis("Mouse Y");
+
+        Vector3 newRotation = transform.localEulerAngles;
+        newRotation.x -= _mouseY * _sensitivity;
+        transform.localEulerAngles = newRotation;
+    }
+}
