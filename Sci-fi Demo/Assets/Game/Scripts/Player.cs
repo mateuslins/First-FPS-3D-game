9bad23da3ab8df1def06dd452a1f36e15181adc5
mateuslins <41246021+mateuslins@users.noreply.github.com>
2020-05-07 14:05:15 -0300

    Nav Mesh and Raycast implemented

--- Sci-fi Demo/Assets/Game/Scripts/Player.cs
+++ Sci-fi Demo/Assets/Game/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _muzzle_flash;
+
     private CharacterController _controller;
 
     [SerializeField]
@@ -15,12 +18,23 @@ public class Player : MonoBehaviour
     void Start()
     {
         _controller = GetComponent<CharacterController>();
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+
+        Shoot();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 
     private void Movement()
@@ -35,5 +49,25 @@ public class Player : MonoBehaviour
         velocity = transform.transform.TransformDirection(velocity);
 
         _controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray rayOrigin = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                Debug.Log("Raycast hit " + hitInfo.transform.name);
+            }
+
+            _muzzle_flash.SetActive(true);
+        }
+        else
+        {
+            _muzzle_flash.SetActive(false);
+        }
     }
 }
